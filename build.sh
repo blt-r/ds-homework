3b2a69fc826c5ee5b73c9ebd444ed7bc5c4d305c
blt__ <www.edav@gmail.com>
2024-10-16 21:18:06 +0400

    Add compiler flag to include debug info

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 CXX=clang++
-CXXFLAGS="-Wall -Wextra -std=c++20 -fsanitize=address -O2"
+CXXFLAGS="-Wall -Wextra -std=c++20 -fsanitize=address -O2 -g"
 
 $CXX $CXXFLAGS main.cpp -o main
